c75f9942b3bbe6cae08fd24bd79fbadea7aeebbe
unknown <tomasfunesrubio@gmail.com>
2023-09-16 16:29:00 -0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace _2019_11_12_CortedeControl_P1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(24, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(726, 617);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Vendedor";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Producto";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Cantidad";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Venta";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Total Vendedor";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(756, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ejecutar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 688);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2019_11_12_CortedeControl_P1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Columns[3].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[4].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool _Agrega1 = false;
+                bool _Agrega2 = false;
+                decimal _TotalGeneral = 0;
+                decimal _TotalVendedor = 0;
+                decimal _TotalProducto = 0;
+                int _TotalCantidadGeneral = 0;
+                int _TotalCantidad = 0;
+                int _CantidadProducto = 0;
+                // Se verifica la existencia del archivo.
+                if (File.Exists("Ventas.txt"))
+                {
+                    // Se genera un StreamReader para leer el archivo.
+                    using (StreamReader sr = new StreamReader("Ventas.txt"))
+                    {
+                        string _S = sr.ReadLine();  
+                        string[] _D = _S.Split(new char[] { ',' });
+
+                        while (_S != null)
+                        {
+                          
+                            string _Vendedor = _D[0];
+                            dataGridView1.Rows.Add(new string[] { _D[0] });
+                            while (_Vendedor == _D[0] && _S!=null)
+                            {           
+                                string _Producto = _D[1];
+                                if (_Agrega1)
+                                    dataGridView1.Rows.Add(new string[] { "", _D[1] });
+                                else
+                                { 
+                                    dataGridView1.Rows[dataGridView1.Rows.GetLastRow(DataGridViewElementStates.None)].Cells[1].Value = _D[1]; 
+                                    _Agrega1 = true; 
+                                }
+
+
+                                while (_Vendedor == _D[0] && _Producto == _D[1] && _S!=null)
+                                {
+                                    _TotalProducto += decimal.Parse(_D[3]);
+                                    _TotalVendedor += decimal.Parse(_D[3]);
+                                    _TotalGeneral += decimal.Parse(_D[3]);
+                                    _CantidadProducto += int.Parse(_D[2]);
+                                    _TotalCantidad += int.Parse(_D[2]);
+                                    _TotalCantidadGeneral += int.Parse(_D[2]);
+                                    if (_Agrega2)
+                                        dataGridView1.Rows.Add(new string[] { "", "", _D[2], _D[3] });
+                                    else
+                                    {
+                                        dataGridView1.Rows[dataGridView1.Rows.GetLastRow(DataGridViewElementStates.None)].Cells[2].Value = _D[2];
+                                        dataGridView1.Rows[dataGridView1.Rows.GetLastRow(DataGridViewElementStates.None)].Cells[3].Value = _D[3];
+                                        _Agrega2 = true;
+                                    }
+                                   _S = sr.ReadLine();
+                                    if (_S != null) { _D = _S.Split(new char[] { ',' }); }
+                                 
+                                }                                
+                                
+                                dataGridView1.Rows.Add(new string[] { "","Sub Tot =>", _CantidadProducto.ToString(),_TotalProducto.ToString()});
+                                dataGridView1.Rows[dataGridView1.Rows.GetLastRow(DataGridViewElementStates.None)].DefaultCellStyle.BackColor = Color.AliceBlue;
+                                dataGridView1.Rows.Add(1);
+                                _TotalProducto = 0;
+                                _CantidadProducto = 0;
+                                _Agrega2 = false;
+                            }
+                            dataGridView1.Rows.RemoveAt(dataGridView1.Rows.Count - 1);
+                            dataGridView1.Rows.Add(new string[] { "", "Cant Vendida =>", _TotalCantidad.ToString(),"Tot Vend =>", _TotalVendedor.ToString() });
+                            dataGridView1.Rows[dataGridView1.Rows.GetLastRow(DataGridViewElementStates.None)].DefaultCellStyle.BackColor = Color.Aquamarine;
+                            dataGridView1.Rows.Add(1);
+                            _TotalVendedor = 0; 
+                            _TotalCantidad = 0;
+                            _Agrega1 = false;
+                        }
+                       
+                        dataGridView1.Rows.Add(new string[] { "", "", _TotalCantidadGeneral.ToString(), "Tot Vendido =>", _TotalGeneral.ToString() });
+                        dataGridView1.Rows[dataGridView1.Rows.GetLastRow(DataGridViewElementStates.None)].DefaultCellStyle.BackColor = Color.Cyan;
+
+                        _TotalGeneral = 0;
+                        _TotalCantidadGeneral = 0;
+                    }
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            }
+    }
+}
